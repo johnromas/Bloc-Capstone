a72857a823f288e1c1dc5aa74659088f39956a09
John Romas <john.romas87@gmail.com>
2014-04-14 18:02:11 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GAL::Application.config.secret_key_base = 'bc6b8406aca8c4ca919fea786fa31d25827cf29c959c54d697ebde0b859cf5eb074994adb9cbf8268d9100579f9498f90de4f06df441d38ac4302ab17e47cd33'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GAL::Application.config.session_store :cookie_store, key: '_GAL_session'
